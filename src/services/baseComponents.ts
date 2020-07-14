3608fc5cd87bbd6d42c22da735a024d1fbbea113
wiseadme <wiseadme@gmail.com>
2020-07-14 18:55:10 +0300

    correct

--- src/services/baseComponents.ts
+++ src/services/baseComponents.ts
@@ -1,29 +1,13 @@
 import { VueConstructor } from 'vue'
-import VButton from '@/components/VButton'
 import { withFunctional } from '@/utils/hoc'
+
+// components
+import VButton from '@/components/VButton'
 
 export default {
   install(Vue: VueConstructor) {
-    const func = withFunctional(VButton)
-    console.log(func)
     Vue.component('VButton', withFunctional(VButton))
   }
 }
 
-
-// export default function () {
-//   // @ts-ignore
-//   const requireComponent = require.context('@/components/', true, /\.ts$/)
-//
-//   requireComponent.keys().forEach((fileName: string) => {
-//     let baseComponentConfig = requireComponent(fileName)
-//     baseComponentConfig = baseComponentConfig.default || baseComponentConfig
-//
-//     const baseComponentName: string =
-//       baseComponentConfig.name ||
-//       fileName.replace(/^.+\//, '').replace(/\.\w+$/, '')
-//
-//     Vue.component(baseComponentName, baseComponentConfig)
-//   })
-// }
 
