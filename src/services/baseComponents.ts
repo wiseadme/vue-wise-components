57612891fc34a1b402056f371d9f9c17bd9f833f
wiseadme <wiseadme@gmail.com>
2020-07-14 18:58:53 +0300

    correct

--- src/services/baseComponents.ts
+++ src/services/baseComponents.ts
@@ -4,9 +4,16 @@ import { withFunctional } from '@/utils/hoc'
 // components
 import VButton from '@/components/VButton'
 
+const components: object = {
+  VButton
+}
+
 export default {
   install(Vue: VueConstructor) {
-    Vue.component('VButton', withFunctional(VButton))
+    Object.keys(components).forEach(key => {
+      // @ts-ignore
+      Vue.component(key, withFunctional(components[key]))
+    })
   }
 }
 
